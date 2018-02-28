f46de37b4048910a4978502330751e059ae947bc
Katy Thomas <katythomas@Katys-MacBook-Pro.local>
2018-02-28 09:24:22 -0500

    Added book detail, routing components

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { IBook } from '../books/book';
+import { BooksService } from '../books/books.service';
+
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+
+export class BooksComponent implements OnInit {
+  books: IBook[] = [];
+  selectedBook: IBook;
+
+  constructor(private _booksService: BooksService) { }
+
+  ngOnInit() {
+    this._booksService.getBooks()
+      .subscribe(books => {
+        this.books = books;
+      },
+        error => console.log(error)
+      );
+  }
+
+  onSelect(book: IBook): void {
+    this.selectedBook = book;
+  }
+}
